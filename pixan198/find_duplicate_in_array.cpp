cf99f6ad31d974e37ef79e0fd65e9f811e8c88db
Ashish Chawda <56932286+pixan198@users.noreply.github.com>
2020-08-04 15:02:24 +0530

    Update find_duplicate_in_array.cpp :zap:

--- pixan198/find_duplicate_in_array.cpp
+++ pixan198/find_duplicate_in_array.cpp
@@ -1,30 +1,55 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <ctime>
 
 //using namespace std;
 
 using std::cin;
 using std::cout;
+using std::endl;
 using std::vector;
-using std::swap;
-using std::sort;
+
 
 int repeatedNumber(const vector<int> &A) {
     
-    vector<int> ret(A.size()-1, 0);
+    vector<int> count(A.size()-1, 0);
     
     for(int i=0; i<A.size(); i++){
-        ret[A[i]-1]++;
-        if(ret[A[i]-1]>1) return A[i];
+        count[A[i]-1]++;
+        if(count[A[i]-1]>1) return A[i];
     }
     return -1;
 }
+int repeatedNumberOptimized(vector<int> &A) 
+{ 
+    int Tortoise = A[0]; 
+    int Hare = A[0]; 
+    
+    while (1) { 
+  
+        Tortoise = A[Tortoise]; 
+        Hare = A[A[Hare]];
+        
+        if (Tortoise == Hare) 
+            break; 
+    } 
+  
+    Tortoise = A[0]; 
+    
+    while (Tortoise != Hare) { 
+        Tortoise = A[Tortoise]; 
+        Hare = A[Hare]; 
+    } 
+   
+    return Tortoise; 
+} 
 
 int main(){
   
   int n, temp;
   vector<int> A;
+  clock_t start, end;
   
   cin >> n;
   
@@ -32,8 +57,16 @@ int main(){
     cin >> temp;
     A.push_back(temp);
   }
+	start = clock();
+    cout << repeatedNumber(A) << endl;
+    end = clock();
+    cout << "Runtime(normal) = " << double(end - start) / double(CLOCKS_PER_SEC) << endl;
+    
+  	start = clock();
+    cout << repeatedNumberOptimized(A) << endl;
+    end = clock();
+    cout << "Runtime(normal) = " << double(end - start) / double(CLOCKS_PER_SEC) << endl;
   
-  cout << repeatedNumber(A);
   
   return 0;
 }
