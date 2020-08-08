57e74181fff5a81b9831a90f17dbd031e8ffbfa3
Himanshi2016 <67581608+Himanshi2016@users.noreply.github.com>
2020-08-08 16:58:57 +0530

    Update DelMid.cpp

--- Himanshi/DelMid.cpp
+++ Himanshi/DelMid.cpp
@@ -1,6 +1,57 @@
 //Question Link
 //https://practice.geeksforgeeks.org/problems/delete-middle-of-linked-list/1
 
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+ int data;
+ struct Node* next;
+  
+ Node(int x)
+ {data=x;
+  next=Null;
+ }
+  
+};
+
+struct Node *deleteMid(struct Node *head);
+Void printList(Node *node)
+{
+  while(node!=Null)
+  { cout << node->data << " ";
+    node =  node->next;
+  }
+  cout << "/n" ;
+}
+
+int main()
+{
+  int t;
+  cin >> t;
+  while(t--)
+  { 
+    int n;
+    cin >> n;
+    int data;
+    cin >> data;
+    struct Node *head=new Node(data);
+    struct Node *tail=head;
+    for(int i=0;i<n-1;++i)
+    {
+      cin >> data;
+      tail->next=new Node(data);
+      tail=tail->next;
+    }
+    head=deleteMid(head);
+    printList(head);
+  } 
+      
+   return 0;   
+  
+}
+
 Node* deleteMid(Node* head)
 {
 
